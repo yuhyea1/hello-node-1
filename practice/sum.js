82326431f2225aa42dc1b6cecd35f33d572ce332
ashleylai <ashleylai58@gmail.com>
2021-09-19 16:24:25 +0800

    add sum.js

---
+++ practice/sum.js
@@ -0,0 +1,9 @@
+console.log("hello world!");
+
+function sum(param) {
+  // TODO: 請從 1 + 2 + 3 + .... + param
+}
+
+console.log(sum(3)); // 6
+console.log(sum(6)); // 21
+console.log(sum(10)); // 55
